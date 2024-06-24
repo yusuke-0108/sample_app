3c53a939b28f811dcfdcdaa80bdb7b964f3a9455
yusuke-0108 <ikuta.yusuke.a9@elms.hokudai.ac.jp>
2024-06-25 01:02:17 +0900

    Add edit update action

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -19,6 +19,10 @@ module SessionsHelper
         end
     end
 
+    def current_user?(user)
+      user && user == current_user
+    end
+
     # 永続的セッションのためにユーザーをデータベースに記憶する
     def remember(user)
         user.remember
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_22_191310) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_24_154858) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
