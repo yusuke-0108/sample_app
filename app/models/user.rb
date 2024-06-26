4ebe76004e1ba313a2bf7bbadfdea92697211d35
yusuke-0108 <ikuta.yusuke.a9@elms.hokudai.ac.jp>
2024-06-26 20:44:47 +0900

    Add Integrate test of users_index and users_show

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,13 +45,22 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 
-  def downcase_email
-    self.email = email.downcase
+  def activate
+    update_columns(activated: true, activated_at: Time.zone.now)
   end
 
-  def create_activation_digest
-    self.activation_token = User.new_token
-    self.activation_digest = User.digest(activation_token)
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
   end
+
+  private
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
   
