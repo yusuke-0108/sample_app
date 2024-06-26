3be8b9931d802d1b0ffa7dd6938f476b165f1134
yusuke-0108 <ikuta.yusuke.a9@elms.hokudai.ac.jp>
2024-06-26 19:49:40 +0900

    Refactoring test of users signup and log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: params[:session][:email].downcase)
-    if @user&.authenticate(params[:session][:password])
-      if @user.activated?
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      if user.activated?
         forwarding_url = session[:forwarding_url]
         reset_session
         params[:session][:remember_me] == '1' ? remember(user) : forget(user)
@@ -25,8 +25,9 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out  if logged_in?
+    log_out if logged_in?
     redirect_to root_url, status: :see_other
   end
 end
+
 
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -80,18 +80,21 @@ class LogoutTest < Logout
 end
 
 class RememberingTest < UsersLogin
+
   test "login with remembering" do
     log_in_as(@user, remember_me: '1')
-    assert_equal  cookies[:remember_token], assigns(:user).remember_token
+    assert_not cookies[:remember_token].blank?
   end
 
   test "login without remembering" do
+    # Cookieを保存してログイン
     log_in_as(@user, remember_me: '1')
-    delete logout_path
+    # Cookieが削除されていることを検証してからログイン
     log_in_as(@user, remember_me: '0')
     assert cookies[:remember_token].blank?
   end
 end
+
 
 
 
