5ee1583a9dd8e7a06df9577699349125bc3d22cc
yusuke-0108 <ikuta.yusuke.a9@elms.hokudai.ac.jp>
2024-06-30 02:14:52 +0900

    Add following and followes pages

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,6 +2,10 @@ require "test_helper"
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
   
+  def setup
+    @user = users(:michael)
+  end
+
   test "layout links" do
     get root_path
     assert_template  'static_pages/home'
@@ -15,5 +19,12 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 
     # get signup_path
     # assert_select "title",  full_title("Sign up")
+  end
+
+  test "count relationships" do
+    log_in_as(@user)
+    get root_path
+    assert_match @user.active_relationships.count.to_s, response.body
+    assert_match @user.passive_relationships.count.to_s, response.body
   end
 end
--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -18,5 +18,7 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     @user.microposts.paginate(page: 1).each do |micropost|
       assert_match micropost.context, response.body
     end
+    assert_match @user.active_relationships.count.to_s, response.body
+    assert_match @user.passive_relationships.count.to_s, response.body
   end
 end
