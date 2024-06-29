b0f59af93322443a784c27439d9065c39b518db5
yusuke-0108 <ikuta.yusuke.a9@elms.hokudai.ac.jp>
2024-06-29 10:46:16 +0900

    All associated microposts when the user is destroyed

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  def setup    
+  def setup
     @user = User.new(name: "Example User", email: "user@example.com",
                       password: "foobar",  password_confirmation: "foobar")
   end
@@ -73,5 +73,13 @@ class UserTest < ActiveSupport::TestCase
 
   test "authenticated? should retuen false for a user with nil digest" do
     assert_not @user.authenticated?(:remember, '')
+  end
+
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(context: "Lorem ipsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
   end
 end
