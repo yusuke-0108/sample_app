3a82bd5295bc9676705f685567bd364560d495fb
yusuke-0108 <ikuta.yusuke.a9@elms.hokudai.ac.jp>
2024-06-29 20:20:10 +0900

    Add integration test for feed display of micropost

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
     before_action :logged_in_user, only: [:create, :destroy]
+    before_action :correct_user,   only: :destroy
 
     def create
         @micropost = current_user.microposts.build(micropost_params)
@@ -13,10 +14,22 @@ class MicropostsController < ApplicationController
     end
 
     def destroy
+        @micropost.destroy
+        flash[:success] = "Micropost deleted"
+        if request.referrer.nil?
+            redirect_to root_url, status: :see_other
+        else
+            redirect_to request.referrer, status: :see_other
+        end
     end
 
     private
         def micropost_params
             params.require(:micropost).permit(:context)
+        end
+
+        def correct_user
+            @micropost = current_user.microposts.find_by(id: params[:id])
+            redirect_to root_url, status: :see_other if @micropost.nil?
         end
 end
